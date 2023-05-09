9c7782128901715f53cee24adc4650ea4af34097
Goutamvxd <103603061+Goutamvxd@users.noreply.github.com>
2023-05-09 22:30:57 +0530

    Add files via upload
    Initial commit

---
+++ restful-web-services/target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into user_details(id,birth_date,name) values(10001,current_date(),'Goutam');
+insert into user_details(id,birth_date,name) values(10002,current_date(),'Anil');
+insert into post(id,description,user_id) values(20001,'I want to learn AWS',10001);
+insert into post(id,description,user_id) values(20002,'I want to learn DevOps',10002);
